618e20368c64f1b0002eb190093a3165130d8468
Mathias Hadji <mathias.hadji@gmail.com>
2021-02-15 17:07:06 +0100

    Modification de la route POST des sauces pour prendre en compte la publication d'images dans la création d'une nouvelle sauce

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
+const multer = require('../middleware/multer-config');
 
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauce);
-router.post('/', auth, sauceCtrl.createSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
