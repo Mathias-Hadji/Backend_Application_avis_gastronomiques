c4bc90ffa77cbbc7929ed2b3579d034487986fd4
Mathias Hadji <mathias.hadji@gmail.com>
2021-02-15 11:19:30 +0100

    Création des routes pour l'enregistrement d'un nouvel utilisateur et de la connexion d'un utilisateur existant. Création du model mongoose d'un utilisateur. Création de la structure de base du controller user.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const userRoutes = require('./routes/user');
+
 
 const app = express();
+
+
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+
+exports.signup = (req, res, next) => {
+
+};
+
+
+exports.login = (req, res, next) => {
+
+};
