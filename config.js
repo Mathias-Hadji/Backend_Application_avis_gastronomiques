7bcbb595f6434270cf26199a828309a8f10bcf4e
Mathias Hadji <mathias.hadji@gmail.com>
2021-03-08 14:51:30 +0100

    Le fichier config.js a été retiré du gitignore pour l'évaluation OC P6

---
+++ config.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+    mongoUri: 'mongodb+srv://mh01:bVXsLcAalxMoxfGI@cluster0.raiyj.mongodb.net/DB-APP-Avis-Gastronomique?retryWrites=true&w=majority'
+}
